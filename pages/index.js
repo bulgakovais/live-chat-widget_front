2bf3a1896639f6fcb7c6bcbf8c137269fd04c238
Irina <irina2358@yandex.ru>
2022-10-21 17:06:04 +0300

    форма для отправки сообщения

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,46 @@ export default function HomePage() {
         <meta name="description" content="Виджет для техподдержки с базой знаний(F.A.Q.) и чатом" />
         <meta charset="utf-8" />
       </Head>
+      <main>
+        <Box
+          sx={{
+            width: 500,
+            height: 700,
+            position: 'relative',
+            backgroundColor: '#dde3f0',
+          }}>
 
+          <Box
+            component="form"
+            sx={{
+              '& .MuiTextField-root': { m: '2em', width: '80%', position: 'absolute', bottom: '5rem' },
+            }}
+            noValidate
+            autoComplete="off"
+          >
+
+            <TextField
+              id="standard-textarea"
+              label="Напишите сообщение"
+              placeholder=""
+              multiline
+              variant="standard"
+            />
+
+            <Button variant="outlined"
+              sx={{
+                position: 'absolute',
+                bottom: '1em',
+                margin: '2em',
+                '&:hover': {
+                  backgroundColor: '#ebf0ff',
+                  opacity: [0.9, 0.8, 0.7],
+                },
+              }}
+            >Отправить</Button>
+          </Box>
+        </Box>
+      </main >
     </div >
 
   )
