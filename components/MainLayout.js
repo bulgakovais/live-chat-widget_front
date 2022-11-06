9a1f0ce62f4f0ea39bb15e0d822391e3ed1bb76d
Victoria <v.shurkalina@gmail.com>
2022-11-06 22:13:16 +0400

    faq page, layout added, store created

---
+++ components/MainLayout.js
@@ -0,0 +1,49 @@
+import Link from 'next/link'
+import Head from 'next/head'
+import { Box } from '@mui/material'
+import styles from '../styles/MainLayout.module.css'
+import SupportAgentIcon from '@mui/icons-material/SupportAgent';
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
+
+export function MainLayout ({ children }) {
+    return (<> 
+      <Head>
+        <title>Live-chat</title>
+        <meta name="description" content="Виджет для техподдержки с базой знаний(F.A.Q.) и чатом" />
+        <meta charSet="utf-8" />
+      </Head> 
+      <div className={styles.container}>
+
+    <Box
+          sx={{
+            width: 500,
+            height: 700,
+            position: 'relative',
+            backgroundColor: '#dde3f0',
+          }}>
+        <nav>
+            <Link href="/"><a><span className='icon'><SupportAgentIcon  sx={{fontSize:'35px'}}/></span></a></Link>
+            <Link href="/faq"><a><span className='icon'><HelpOutlineIcon sx={{fontSize:'35px'}}/></span></a></Link>
+         </nav>
+         {children}
+         </Box>
+    
+    </div >
+    <style jsx>{`
+        nav {
+            display: flex;
+            justify-content: center;
+            padding-top: 2rem;
+        }
+        nav a {
+            margin-right: 2rem
+        }
+        .icon {
+            color:black;
+        }
+        .icon:hover {
+            color: #F9F6EE;
+        }
+    `}</style>
+    </>)
+}
--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,10 @@
 import { Box, Button, TextField } from '@mui/material'
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import { MainLayout } from '../components/MainLayout'
 
 export default function HomePage() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Live-chat</title>
-        <meta name="description" content="Виджет для техподдержки с базой знаний(F.A.Q.) и чатом" />
-        <meta charset="utf-8" />
-      </Head>
+    <MainLayout>
       <main>
-        <Box
-          sx={{
-            width: 500,
-            height: 700,
-            position: 'relative',
-            backgroundColor: '#dde3f0',
-          }}>
-
           <Box
             component="form"
             sx={{
@@ -48,9 +34,7 @@ export default function HomePage() {
               }}
             >Отправить</Button>
           </Box>
-        </Box>
-      </main >
-    </div >
-
+          </main>
+          </MainLayout>
   )
 }
