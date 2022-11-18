a0975e604ba71a53f6fe95158217c5cc4f3a079f
Irina <irina2358@yandex.ru>
2022-11-18 17:02:43 +0300

    заполнение компонента QuestionForm

--- components/QuestionForm.js
+++ components/QuestionForm.js
@@ -3,88 +3,121 @@ import { useForm } from "react-hook-form"
 import { API } from '../utils/constants'
 import styles from '../styles/MainLayout.module.css'
 
-export function QuestionForm () {
-    const {
-        register,
-        formState: {
-          errors,
-          isValid
+export function QuestionForm() {
+
+  const {
+    register,
+    formState: {
+      errors,
+      isValid
+    },
+    handleSubmit,
+    reset,
+  } = useForm({
+    mode: 'onBlur'
+  })
+  // reset - очистка формы
+  // isValid - неактивная кнопка при незаполненной форме
+
+  const regexEmail = /^(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$/
+  const regexPhone = /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/
+
+  // Проверка полей email и phone
+  function contactValue(value) {
+
+    if (regexPhone.test(value) || regexEmail.test(value))
+      return true
+    else return false
+  }
+
+
+  // В зависимости от введенного контакта, отправляется поле email || phone
+  function dataGeneration(data) {
+    const newData = {}
+
+    if (regexEmail.test(data.contact)) {
+
+      newData.text = data.text
+      newData.email = data.contact
+      newData.name = data.name
+
+      return newData
+
+    } else {
+      newData.text = data.text
+      newData.phone = data.contact
+      newData.name = data.name
+
+      return newData
+    }
+  }
+
+
+  const onSubmit = async (data) => {
+    const newData = dataGeneration(data)
+
+    try {
+      const response = await fetch(`${API}/question`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json;charset=utf-8'
         },
-        handleSubmit,
-        reset,
-      } = useForm({
-        mode: 'onBlur'
+        body: JSON.stringify(newData)
       })
-      // reset - очистка формы
-      // isValid - неактивная кнопка при незаполненной форме
-    
-      const onSubmit = async (data) => {
-    
-        console.log(JSON.stringify(data));
-    
-        try {
-          const response = await fetch(`${API}/question`, {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json;charset=utf-8'
-            },
-            body: JSON.stringify(data)
-          })
-          let result = await response.json();
-          console.log(result);
-        } catch {
-          console.log('error POST request')
-        }
-        reset()
-    
-      };
-    return (
-        <>
-          <form id='messageForm' onSubmit={handleSubmit(onSubmit)} className={styles.form}>
+      let result = await response.json();
+      console.log(result);
 
-            <Box><TextField id="outlined-basic" label="Name" variant="outlined"
-              {...register("name", { required: 'Напишите как к Вам обращаться' })}
+    } catch {
+      console.log('error POST request')
+    }
+    reset()
 
-            />
-              <div style={{ height: 20, color: 'red', fontSize: '12px' }}>
-                {errors?.name && <p>{errors?.name?.message || "Error!"}</p>}
-              </div></Box>
+  };
 
-            <Box><TextField id="outlined-basic" label="Email" variant="outlined" {...register("email", {
-              required: 'Напишите Ваш email.',
-              pattern: {
-                value: /^(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$/,
-                message: 'Некорректно набран email'
-              }
+
+  return (
+    <>
+      <Box
+        sx={{
+          margin: '10% 0',
+          height: '100%',
+          backgroundColor: '#dde3f0',
+        }}>
+        <form id='messageForm' onSubmit={handleSubmit(onSubmit)} className={styles.form}>
+
+          <Box><TextField id="outlined-basic" label="Имя" variant="outlined" sx={{ width: '100%', boxSizing: 'content-box' }}
+            {...register("name", { required: 'Напишите как к Вам обращаться' })}
+
+          />
+            <div style={{ height: 20, color: 'red', fontSize: '12px' }}>
+              {errors?.name && <p>{errors?.name?.message || "Error!"}</p>}
+            </div></Box>
+
+          <Box>
+            <TextField id='outlined-basic' label="Номер телефона или email" sx={{ width: '100%', boxSizing: 'content-box' }} variant="outlined" {...register('contact', {
+              required: 'Напишите Ваш номер телефона или email.',
+              validate: value => contactValue(value)
             })}
-            /> <div style={{ height: 20, color: 'red', fontSize: '12px' }}>
-                {errors?.email && <p>{errors?.email?.message || "Error!"}</p>}
-              </div></Box>
-            <Box><TextField id="outlined-basic" label="Phone" variant="outlined"  {...register("phone", {
-              required: 'Напишите Ваш номер телефона.',
-              minLength: {
-                value: 11,
-                message: 'Минимум 11 символов.'
-              }
-            })}
-            /> <div style={{ height: 20, color: 'red', fontSize: '12px' }}>
-                {errors?.phone && <p>{errors?.phone?.message || "Error!"}</p>}
-              </div></Box>
+            /> <div style={{ height: '100%', minHeight: '20px', color: 'red', fontSize: '12px' }}>
+              {errors?.contact && <p>{errors?.contact?.message || 'Некорректно набрана контактная информация. Обратите внимание на формат email: example@example.ex, номер телефона - 11 цифр.'}</p>}
+            </div>
 
-            <Box><TextField
-              id="outlined-multiline-static"
-              label="Message"
-              multiline
-              rows={4}
+          </Box>
+          <Box><TextField
+            id="outlined-multiline-static"
+            label="Сообщение"
+            multiline
+            rows={4}
+            sx={{ width: '100%', boxSizing: 'content-box' }}
+            {...register("text", { required: 'Напишите Ваш вопрос.' })}
 
-              {...register("text", { required: 'Напишите Ваш вопрос.' })}
+          /> <div style={{ height: 20, color: 'red', fontSize: '12px' }}>
+              {errors?.text && <p>{errors?.text?.message || "Error!"}</p>}
+            </div></Box>
 
-            /> <div style={{ height: 20, color: 'red', fontSize: '12px' }}>
-                {errors?.text && <p>{errors?.text?.message || "Error!"}</p>}
-              </div></Box>
-
-            <Button variant="outlined" type="submit" disabled={!isValid}>Send</Button>
-          </form>
-        </>
-    )
+          <Button sx={{ boxSizing: 'content-box', width: 'fit-content', alignSelf: 'flex-end' }} variant="outlined" type="submit" disabled={!isValid}>Отправить</Button>
+        </form>
+      </Box>
+    </>
+  )
 }
